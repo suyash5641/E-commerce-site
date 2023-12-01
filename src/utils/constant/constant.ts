8408280df35ef7b06c8098955ed4d79b79324fc6
suyash5641 <suyashjai05@gmail.com>
2023-12-02 04:18:15 +0530

    fix error

--- src/utils/constant/constant.ts
+++ src/utils/constant/constant.ts
@@ -1,2 +1,2 @@
 export const stripe_key = process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY ?? '';
-export const BASE_URL=process.env.REACT_APP_API_URI;
+export const BASE_URL="https://ecommerce-server-ccwu.onrender.com/api"
